b5b6ccba86f47f583a0c55e2b8736770af72e2a8
danial137 <danial79fakhrabadi@gmail.com>
2025-08-12 16:08:19 +0330

    update

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import { auth } from "@clerk/nextjs/server";
-
+import { redirect } from 'next/navigation';
 interface SetupLayoutProps { 
     children: React.ReactNode;
 
 }
 const SetupLayout = async ({ children }: SetupLayoutProps) => {
   const { userId } = await auth()
+
+     if (!userId) {
+          redirect('/');
+  }
+  
   return (
       <div>
           {children}
